fd0a0c28a5e987dab53b8ddf08f34f4084628419
wangweifeng <120055563@qq.com>
2025-01-24 16:27:13 +0800

    XX

--- SOM_DEVELOP_TOOL/Clear.bat
+++ SOM_DEVELOP_TOOL/Clear.bat
@@ -2,5 +2,7 @@ rd /s /q .\SOM_DEVELOP_TOOL\obj
 rd /s /q .\SOM_DEVELOP_TOOL\bin
 rd /s /q .\XY_IPC\obj
 rd /s /q .\XY_IPC\bin
+rd /s /q .\Uninstall\obj
+rd /s /q .\Uninstall\bin
 rd /s /q .\.vs
 pause
